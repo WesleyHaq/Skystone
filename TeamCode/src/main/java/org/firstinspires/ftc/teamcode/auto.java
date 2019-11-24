451afe3a8b66aa13311024f05cbe78e789ed650b
WoodlandsRobotics <31572467+phantom12857@users.noreply.github.com>
2019-11-24 17:15:17 -0600

    h

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+@Autonomous(name="tensorflow",group="autonomous")
+public class auto extends LinearOpMode{
+    MecanumConfig robot = new MecanumConfig();
+    MecanumDrive drive = new MecanumDrive(robot);
+
+
+    private static final String TFOD_MODEL_ASSET = "model.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Skystone";
+//  private static final String LABEL_SECOND_ELEMENT = "Stone";
+
+    private static final String VUFORIA_KEY =
+            "AahhGez/////AAABmSIunC5QeUbDmqSuN/JVvK5jcN03J60dszlHmZqGkfuUkUVQsH4/YxQk1A+Rg/DlMP1LwU5U/g4EW5+j6271bFI0ZWGQScblmtv/MVEAgzJqZdZkHl/ZS8qgIDkPDzKJLCJwz9qtN6fKZJ5FC0uxuQ9vHblm5dyr0iIAUM78c6Wc2fzqDmTm3WuOFbBthCKJRJNpfhUn9CyA1Wdev+LzIbotSs++L3a9O4ekv1/NKi5Khujw3L2i1IxY8M2hZlTYfeCn57W4bCxnfSWChH0yNv7G5gk3jyMP5d8pR1XtqDu4kqx+dQ2/Fq+M9Fdun8nfE5peCIVk592Ul+sjhC5Nr8FQZIFfxoAJIAWu3o4QizOb\n";
+
+    private VuforiaLocalizer vuforia;
+
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() {
+
+        robot.init(hardwareMap);
+
+        initVuforia();
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
+
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        if (tfod != null) {
+            tfod.activate();
+        }
+        telemetry.update();
+        /** Wait for the game to begin */
+//        telemetry.addData(">", "Press Play to start op mode");
+//        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+                            drive.angleDrive(90, (recognition.getRight()-100)/100);
+                        }
+                        telemetry.update();
+                    }
+                }
+            }
+        }
+
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+    }
+
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+        telemetry.addData("Init Vuforia", "successful");
+
+    }
+
+    private void initTfod() {
+        try {
+            int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                    "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+            TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+            tfodParameters.minimumConfidence = 0.8;
+            tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+            tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT);
+        } catch (Exception e) {
+            telemetry.addData("exception", e.getMessage());
+
+        }
+
+
+    }
+}
